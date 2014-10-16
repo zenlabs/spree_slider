7f3ed9599a4a700e79668729a838b3e4992bcb12
Carlos Ramos <carakan@gmail.com>
2014-10-16 13:28:23 -0400

    Added field for enable banners

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -1,13 +1,15 @@
 class Spree::Slide < ActiveRecord::Base
-
   has_attached_file :image
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   scope :published, -> { where(published: true).order('position ASC') }
 
+  scope :banners_published, -> { published.where(slide: false) }
+  scope :slides_published, -> { published.where(slide: true) }
+
   belongs_to :product, touch: true
 
   def initialize(attrs = nil)
-    attrs ||= {:published => true}
+    attrs ||= { published: true }
     super
   end
 
